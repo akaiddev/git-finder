3ad8fc72b79a8799a35ddaa9232ae43aa80cb977
Abdur Rahman <akaid.dev@gmail.com>
2022-01-28 19:08:17 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Github Finder App
+
+App to search Github users and display their info. This is part of my React Front To Back 2022 course and is the most up to date version of this project.
+
+## Usage
+
+Rename **_.env.example_** to **_.env_**
+
+You can use the Github API without a personal token, but if you want to use your token, add it to the .env file
+
+Learn how to create a token [here](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+
+### Install Dependencies
+
+```
+npm install
+```
+
+### Run
+
+```
+npm start
+```
+# git-finder
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Alert from './components/layout/Alert'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import { AlertProvider } from './context/alert/AlertContext'
+import { GithubProvider } from './context/github/GithubContext'
+import About from './pages/About'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import User from './pages/User'
+
+function App() {
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
+  )
+}
+
+export default App
